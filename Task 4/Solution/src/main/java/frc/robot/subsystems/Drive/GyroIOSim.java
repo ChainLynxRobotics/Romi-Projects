1369a2a5d7f7168908b27bd1e510464172cf1ce8
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-05-05 16:31:56 -0700

    remove task 4

--- Task 4/Solution/src/main/java/frc/robot/subsystems/Drive/GyroIOSim.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.subsystems.Drive;
-
-public class GyroIOSim implements GyroIO {
-    public GyroIOSim() {}
-
-    @Override
-    public void updateInputs(GyroIOInputs inputs) {}
-    public void resetGyro() {}
-}
